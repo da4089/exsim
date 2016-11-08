1ec56462c2df3bf48d110d084a6b348107f635a3
David Arnold <d@0x1.org>
2016-11-08 20:20:16 +1100

    Basic heartbeating working with Whisperer.
    Not doing proper timeouts yet, just blindly sending.

--- exsim/fix.py
+++ exsim/fix.py
@@ -1,12 +1,15 @@
 #! /usr/bin/python
 
+import datetime
 import logging
 import select
 import socket
+import time
 
-from fix_protocol import FixParser, FixMessage
+from fix_protocol import FixParser, FixMessage, print_fix
 
 logging.basicConfig(level=logging.DEBUG)
+
 
 
 class Session(object):
@@ -16,6 +19,18 @@ class Session(object):
         self._address = addr
         self._parser = FixParser()
         self._gateway = None
+
+        # Next expected sequence number.
+        self._in_seq = 1
+
+        # Sequence number to be used for next sent message.
+        self._out_seq = 1
+
+        # Interval before sending TestRequest or Heartbeat messages.
+        self._heartbeat_interval = 30 * 1000
+
+        # Outstanding test request identifiers.
+        self._test_requests = {}
         return
 
     def set_gateway(self, gateway):
@@ -53,6 +68,9 @@ class Session(object):
             elif t == '0':
                 self.handle_heartbeat(fix_msg)
 
+            elif t == '1':
+                self.handle_test_request(fix_msg)
+
             elif t == 'A':
                 self.handle_logon(fix_msg)
 
@@ -68,11 +86,48 @@ class Session(object):
     def handle_new_order(self, fix_msg):
         return
 
+    def handle_test_request(self, fix_msg):
+        test_request_id = fix_msg.get(112)
+        logging.debug("Received TestRequest, id = %s" % str(test_request_id))
+
+        self.send_heartbeat(test_request_id)
+        return
+
     def handle_heartbeat(self, fix_msg):
         return
 
     def handle_logon(self, fix_msg):
-        logging.info("Logon")
+        # Check Sender and TargetCompID
+        # Check username/password
+
+        # Send response
+        resp = FixMessage()
+        resp.set_message_type('A')  # Logon
+        resp.append_pair(34, self.next_seq())
+        resp.append_pair(49, fix_msg.get(56))  # Sender
+        resp.append_pair(56, fix_msg.get(49))  # Target
+        resp.append_pair(52, self.get_fix_time())
+        resp.append_pair(98, 0)  # No encryption
+        resp.append_pair(108, fix_msg.get(108))  # Heartbeat interval
+        s = resp.encode()
+
+        print "Sending ",
+        print_fix(s)
+
+        self._socket.send(s)
+
+        # Save CompIDs
+        self._peer_compid = fix_msg.get(49)
+        self._my_compid = fix_msg.get(56)
+
+        # Save the heartbeat interval.
+        try:
+            self._heartbeat_interval = int(fix_msg.get(108)) * 1000
+        except ValueError:
+            pass
+
+        # Register timeout for heartbeat.
+        self._gateway.add_timeout(time.time() + self._heartbeat_interval, self.send_heartbeat)
         return
 
     def handle_logout(self, fix_msg):
@@ -81,6 +136,31 @@ class Session(object):
     def send_message(self, message):
         self._socket.send(message)
         return
+
+    def send_heartbeat(self, test_request_id=None):
+        msg = FixMessage()
+        msg.set_message_type('0')  # Heartbeat
+        msg.append_pair(34, self.next_seq())
+        msg.append_pair(49, self._my_compid)
+        msg.append_pair(56, self._peer_compid)
+        msg.append_pair(52, self.get_fix_time())
+
+        if test_request_id:
+            msg.append_pair(112, test_request_id)
+
+        self.send_message(msg.encode())
+
+        logging.debug("Sent heartbeat")
+        return
+
+    def next_seq(self):
+        seq = self._out_seq
+        self._out_seq += 1
+        return seq
+
+    def get_fix_time(self):
+        return datetime.datetime.utcnow().isoformat('-')[:-3]
+
 
 
 class Endpoint(object):
@@ -102,6 +182,7 @@ class Endpoint(object):
         return self._socket
 
 
+
 class Gateway(object):
 
     def __init__(self):
@@ -110,6 +191,7 @@ class Gateway(object):
         self._sessions = {}  #  socket: session
         self._endpoints = {}  # socket: endpoint
         self._is_running = True
+        self._timeouts = []
         return
 
     def set_engine(self, engine):
@@ -149,17 +231,44 @@ class Gateway(object):
     def run(self):
 
         while self._is_running:
-            # Wait 200ms for something to happen on a socket.
+            now = time.time()
+            while True:
+                if len(self._timeouts) == 0:
+                    break
+
+                expiry, callback = self._timeouts[0]
+                if expiry > now:
+                    break
+
+                callback()
+                self._timeouts.pop(0)
+
+            if len(self._timeouts) > 0:
+                wait = self._timeouts[0][0] - time.time()
+                if wait < 0:
+                    wait = 0
+            else:
+                wait = 1.0
+
             sessions = self.get_sessions()
             endpoints = self.get_endpoints()
 
-            r, w, x = select.select(sessions + endpoints, [], [], 0.2)
+            r, w, x = select.select(sessions + endpoints, [], [], wait)
 
             for s in r:
                 if s in self._endpoints:
                     self.create_session(s)
                 else:
                     msg = self._sessions[s].readable()
+        return
 
+    def add_timeout(self, expiry_time, callback):
+        t = (expiry_time, callback)
+        self._timeouts.append(t)
+        self._timeouts.sort()
+        return
 
+    def delete_timeout(self, expiry_time, callback):
+        t = (expiry_time, callback)
+        self._timeouts.remove(t)
         return
