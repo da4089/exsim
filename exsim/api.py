40444b24d0d58bcc1324885d27e5cde8b6541c66
David Arnold <darnold@marketfactory.com>
2017-01-12 23:15:54 +1100

    Add basic API support.

--- exsim/api.py
+++ exsim/api.py
@@ -1,9 +1,68 @@
 #! /usr/bin/env python
 
+import logging
+import pickle
+import socket
+import struct
+
+logging.basicConfig(level=logging.DEBUG)
+
+
 class API:
     """Client API."""
 
     def __init__(self):
+        self._socket = None
+        self._buffer = ''
+        self._connected = False
         return
 
-    
+    def connect(self, host, port):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host, int(port)))
+        self._connected = True
+        logging.info("Connected: %s:%u" % (host, int(port)))
+        return
+
+    def send(self, request, reply):
+
+        # Send request.
+        body = pickle.dumps(request)
+        header = struct.pack("<L", len(body))
+        self._socket.sendall(header + body)
+        logging.debug("Sent")
+
+        # Wait for reply.
+        while True:
+            data = self._socket.recv(8192)
+            logging.debug("Received")
+            if len(data) == 0:
+                self._connected = False
+                # FIXME: set error in reply
+                return False
+
+            self._buffer.append(data)
+            if len(self._buffer) < 4:
+                continue
+
+            length = struct.unpack(self._buffer[:4], "<L")[0]
+            if len(self._buffer) < length + 4:
+                continue
+
+            tmp = pickle.loads(self._buffer[4:4+length])
+            reply.extend(tmp)
+            self._buffer = self._buffer[4+length:]
+            if len(self._buffer) > 0:
+                # FIXME: report error
+                pass
+                
+            return True
+
+
+if __name__ == "__main__":
+    api = API()
+    api.connect("localhost", 10101)
+
+    request = {'type': 'create_engine'}
+    reply = {}
+    api.send(request, reply)
