50a14cdc5b591758adb377e7a6f9d399f8c0f032
David Arnold <darnold@marketfactory.com>
2017-01-12 23:17:01 +1100

    Remove obsolete code.

--- xs.py
+++ xs.py
@@ -6,25 +6,11 @@ def main():
 
     # Server
     server = exsim.Server()
-    server.listen(11111)
-
+    #server.listen(11111)
     server.run()
 
     return
     
-    # Matching engine.
-    engine = exsim.Engine()
-
-    # FIX gateway.
-    fix_gateway = exsim.fix.Gateway()
-    fix_gateway.set_engine(engine)
-
-    fix_gateway.listen(10155)
-    fix_gateway.listen(10156)
-
-    # Run.
-    fix_gateway.run()
-    return
 
 if __name__ == "__main__":
     main()
