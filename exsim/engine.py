da029164fe208ad2c94412278e53fe43f78cea95
David Arnold <d@0x1.org>
2017-01-15 21:47:11 +1100

    Placeholder methods for engine messaging.

--- exsim/engine.py
+++ exsim/engine.py
@@ -38,15 +38,78 @@ class Engine(object):
             return
 
 
-    def handle_new_order(self, message):
+    def handle_login(self, message):
         return
 
-    def handle_modify_order(self, message):
+    def handle_logout(self, message):
+        return
+
+    def handle_new_order(self, message):
+        """Process new order."""
         return
 
     def handle_cancel_order(self, message):
+        """Process attempt to cancel an open oerder."""
+        return
+
+    def handle_subscribe(self, message):
+        """Process request for streaming prices."""
+        return
+
+    def handle_unsubscribe(self, message):
+        """Process request to cancel streaming prices."""
+        return
+
+    def handle_quote(self, message):
+        """Process new submitted quote."""
+        return
+
+    def handle_quote_delete(self, message):
+        """Process withdrawal of a quote."""
         return
 
 
+
     def publish_message(self, message):
+        return
+
+
+    def send_login_ack(self, message):
+        """Acknowledge successful login."""
+        return
+
+    def send_login_reject(self, message):
+        """Acknowledge unsuccessful login."""
+        return
+
+    def send_quote_ack(self, message):
+        """Acknowledge submitted quote."""
+        return
+
+    def send_quote_delete_ack(self, message):
+        """Acknowledge deletion of quote."""
+        return
+
+    def send_subscribe_ack(self, message):
+        """Acknowledge successful subscription."""
+        return
+
+    def send_subscribe_reject(self, message):
+        """Acknowledge unsuccessful subscription."""
+        return
+
+    def send_market_data_update(self, message):
+        """Publish new quote to subscribers."""
+        return
+
+    def send_order_ack(self, message):
+        """Acknowledge new order."""
+        return
+
+    def send_order_cancelled(self, message):
+        """Report cancellation of remaining order quantity."""
+        return
+
+    def send_order_execution(self, message):
+        """Report execution of some order quantity."""
         return
